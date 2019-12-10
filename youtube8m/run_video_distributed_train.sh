49eb96e5acaf2d1bb6af36fb15a29f390b89d2a0
halhorn <halhorn@halmidi.com>
2019-12-10 03:18:26 +0000

    shell

--- youtube8m/run_video_distributed_train.sh
+++ youtube8m/run_video_distributed_train.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-JOBNAME=youtube8m_$(date -u +%y%m%d_%H%M%S)
+JOBNAME=youtube8m_dist_nocache_$(date -u +%y%m%d_%H%M%S)
 BUCKET=asl-mixi-project-bucket
 OUTDIR=gs://$BUCKET/model/youtube8m/video/auc/$(date -u +%y%m%d_%H%M%S)
 
 gcloud ai-platform jobs submit training $JOBNAME \
-       --region=us-central1 \
-       --module-name=video.task \
+       --region='us-central1' \
        --package-path=$(pwd)/video \
        --job-dir=$OUTDIR \
        --staging-bucket=gs://$BUCKET \
-       --scale-tier=BASIC_GPU \
        --config=$(pwd)/video/hyperparam.yaml \
-       --runtime-version=1.14 \
-       --python-version=3.5 \
        -- \
        --train_data_path=gs://$BUCKET/data/youtube-8m/train/*.tfrecord \
        --eval_data_path=gs://$BUCKET/data/youtube-8m/valid/*.tfrecord \
